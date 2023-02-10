49b265928f72d5927ba37028ecfd312184049718
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2023-02-10 23:43:30 +0200

    Update main.cpp

--- _LAB_1_2sem/main.cpp
+++ _LAB_1_2sem/main.cpp
@@ -1,5 +1,6 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 #include <chrono>
 #include <windows.h>
 #include <sstream>
@@ -9,54 +10,58 @@ class AnimatedText
 {
 private:
     double m_duration = 0;
-    std::string m_text = "";
+    std::string m_text;
 
 public:
-    std::string textik = "";
-    AnimatedText(double duration, std::string text)
+    AnimatedText(const double& duration, std::string text)
     {
-        textik = "";
         m_duration = duration;
         m_text = text;
     }
     double s4et()
     {
-        return (m_duration / size(m_text));
+        return (m_duration) / size(m_text);
     }
 
-    void update(float time, int cur)
-    {   
-        Interaction(cur);
-    }
-
-    void Interaction(int cur)
+    double GetDuration()
     {
-        if (size(m_text) < cur)
-        {
-            exit(0);
-        }
-        std::string sl = "";
-        for (int i = 0; i < cur; i++)
-        {
-            sl += m_text[i];
-        }
-        textik = sl;
-
+        return m_duration;
     }
+
+    std::string update(float time_main)
+    {
+        std::string Out;
+        for (int i = 0; i <= (time_main / s4et()); i++)
+        {
+            Out += m_text[i];
+        }
+        return (Out);
+    }
+
 
 
     ~AnimatedText() {}
-     
+
 };
+
+bool Checking(float timing, double duration)
+{
+    if (timing > duration) 
+    {
+        Sleep(500);
+        exit(0);
+    }
+
+}
 
 int main()
 {
-    int cur = 0;
 
-    sf::Font font;//шрифт 
-    font.loadFromFile("CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
-    sf::Text text("", font, 30);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
-    text.setStyle(sf::Text::Bold);//жирный текст.
+
+    sf::Font font_1;//шрифт 
+    font_1.loadFromFile("CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
+    sf::Text Font("", font_1, 30);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+    Font.setStyle(sf::Text::Bold);//жирный текст.
 
     double duration;
     std::cout << "Duration: ";
@@ -67,39 +72,40 @@ int main()
     a = txt;
     std::getline(std::cin, txt);
     txt = a + txt;
-    AnimatedText Test(duration, txt);
+    AnimatedText Main_text(duration, txt);
 
     sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
 
 
+
     float CurrentFrame = 0;
-    sf::Clock clock;
+    sf::Clock clock_main;
+    sf::Clock clock_restart;
 
     while (window.isOpen())
     {
-        float time = clock.getElapsedTime().asMicroseconds();
-        time = time / 800 / 1000;
+
+        float time_main = clock_main.getElapsedTime().asMicroseconds();
+        float time_restart = clock_restart.getElapsedTime().asMicroseconds();
+        time_main = time_main / 800 / 1000 ;
+        time_restart = time_restart / 800 / 1000;
 
 
         std::ostringstream AnimationOfText;
-        AnimationOfText << Test.textik;
 
+        AnimationOfText << Main_text.update(time_main);
+        Font.setString("Text: " + AnimationOfText.str());
 
-        if (time > Test.s4et()) {
-            cur += 1;
-            Test.update(time, cur);
-            clock.restart();
+        if (time_restart > Main_text.s4et())
+        {
+            std::cout << time_restart << std::endl;
+            clock_restart.restart();
         }
 
-        text.setString("Ваше слово: " + AnimationOfText.str());
-        text.setPosition(50, 150);//задаем позицию текста
-        text.setFillColor(sf::Color::Red);
-        window.draw(text);//рисую этот текст
-    
+        Font.setPosition(50, 150);
+        window.draw(Font);
 
-
-
-
+        Checking(time_main, Main_text.GetDuration());
 
         sf::Event event;
         while (window.pollEvent(event))
@@ -107,11 +113,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
-        window.draw(text);
+        window.draw(Font);
         window.display();
     }
-
     return 0;
 }

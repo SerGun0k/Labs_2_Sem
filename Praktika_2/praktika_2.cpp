9a6364cad02bccb7aa12b8223dbd809b24d60c9a
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2023-06-03 15:19:25 +0200

    Update praktika_2.cpp

--- Praktika_2/praktika_2.cpp
+++ Praktika_2/praktika_2.cpp
@@ -6,27 +6,34 @@ void copyArray(void* source, void* destination, size_t cnt)
 {
 	uint64_t* ps = (uint64_t*)source;
 	uint64_t* pd = (uint64_t*)destination;
-	for (int i = 0; i < cnt / 2; i++) {
+	for (int i = 0; i < cnt; i++) {
 		*(pd++) = *(ps++);
 	}
-
-	if (((cnt * sizeof(int)) % 8) == 4) {
-		uint32_t* ps32res = (uint32_t*)((uintptr_t)source + sizeof(uint64_t) * (cnt / 2));
-		uint32_t* pd32res = (uint32_t*)((uintptr_t)destination + sizeof(uint64_t) * (cnt / 2));
+	if (((cnt * sizeof(int)) % 8) == 4) 
+	{
+		uint32_t* ps32res = (uint32_t*)((uintptr_t)source + sizeof(uint64_t) );
+		uint32_t* pd32res = (uint32_t*)((uintptr_t)destination + sizeof(uint64_t) );
 		*(pd32res) = *(ps32res);
 	}
 
 	if ((cnt * sizeof(int)) % 8 == 0)
 	{
-		uint64_t* psres = (uint64_t*)((uintptr_t)source + sizeof(uint64_t) * (cnt / 2));
-		uint64_t* pdres = (uint64_t*)((uintptr_t)destination + sizeof(uint64_t) * (cnt / 2));
+		uint64_t* psres = (uint64_t*)((uintptr_t)source + sizeof(uint64_t) );
+		uint64_t* pdres = (uint64_t*)((uintptr_t)destination + sizeof(uint64_t) );
 		*(pdres) = *(psres);
+	}
+
+	if ((((cnt * sizeof(int)) % 8) >= 1) && (((cnt * sizeof(int)) % 8) < 3))
+	{
+		char* ps8res = (char*)((uintptr_t)source + sizeof(uint64_t) );
+		char* pd8res = (char*)((uintptr_t)destination + sizeof(uint64_t) );
+		*(pd8res) = *(ps8res);
 	}
 }
 
 int main() {
 	setlocale(LC_ALL, "russian");
-	const int N = 5;
+	const int N = 13;
 	int* A = new int[N];
 	int* B = new int[N];
 

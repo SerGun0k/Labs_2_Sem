a4d511abb7adcbf592c1862c8039f17fe2fbd62c
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2023-06-10 12:42:14 +0200

    Update praktika_2.cpp

--- Praktika_2/praktika_2.cpp
+++ Praktika_2/praktika_2.cpp
@@ -1,58 +1,44 @@
 #include <iostream>
-#include <cstdint>
-
-
-void copyArray(void* source, void* destination, size_t cnt)
+	template <typename T>
+static void COPYARRAY(T* source, T* destination, int size, size_t CAPac)
+	{
+    std::cout<< CAPac;
+		const long long* src1 = (const long long*)source;
+		long long* t_src1 = (long long*)destination;
+		int m = sizeof(CAPac) * size / sizeof(long long);
+		for (int i = 0; i < m; i++)
+        {
+			*(t_src1++) = *(src1++);
+		}
+		char* src2 = (char*)src1;
+		char* t_src2 = (char*)t_src1;
+		m = (sizeof(CAPac) * size) % sizeof(long long);
+		for (int i = 0; i < m; i++)
+		{
+			*(t_src1++) = *(src2++);
+		}
+	}
+int main()
 {
-	uint64_t* ps = (uint64_t*)source;
-	uint64_t* pd = (uint64_t*)destination;
-	for (int i = 0; i < cnt; i++) {
-		*(pd++) = *(ps++);
-	}
-	if (((cnt * sizeof(int)) % 8) == 4) 
-	{
-		uint32_t* ps32res = (uint32_t*)((uintptr_t)source + sizeof(uint64_t) );
-		uint32_t* pd32res = (uint32_t*)((uintptr_t)destination + sizeof(uint64_t) );
-		*(pd32res) = *(ps32res);
-	}
-
-	if ((cnt * sizeof(int)) % 8 == 0)
-	{
-		uint64_t* psres = (uint64_t*)((uintptr_t)source + sizeof(uint64_t) );
-		uint64_t* pdres = (uint64_t*)((uintptr_t)destination + sizeof(uint64_t) );
-		*(pdres) = *(psres);
-	}
-
-	if ((((cnt * sizeof(int)) % 8) >= 1) && (((cnt * sizeof(int)) % 8) <= 3))
-	{
-		char* ps8res = (char*)((uintptr_t)source + sizeof(uint64_t) );
-		char* pd8res = (char*)((uintptr_t)destination + sizeof(uint64_t) );
-		*(pd8res) = *(ps8res);
-	}
-}
-
-int main() {
-	setlocale(LC_ALL, "russian");
-	const int N = 13;
-	int* A = new int[N];
-	int* B = new int[N];
-
-	// Инициализация исходного массива
-	for (int i = 0; i < N; i++) {
-		A[i] = i;
-	}
-
-	// Копирование массива
-	copyArray(A, B, N);
-	// вывод результата копирования
+	const int N = 20;
+	unsigned short int mas_a[N] = { 1,1,1,1,5, 6, 7, 8 };
+    unsigned short int mas_b[N] = { 0 };
+	std::cout << "mas_a: ";
 	for (int i = 0; i < N; i++)
 	{
-		std::cout << B[i] << " ";
+		std::cout << mas_a[i] << " ";
 	}
+	std::cout << std::endl;
+    COPYARRAY(mas_a, mas_b, N,sizeof(mas_a[N]));
+	std::cout << "mas_b: ";
+	for (int i = 0; i < N; i++)
+	{
+		std::cout << mas_b[i] << " ";
+	}
+	std::cout << std::endl;
 
-	// Освобождение памяти
-	delete[] A;
-	delete[] B;
 
+    delete[] mas_a;
+    delete[] mas_b;
 	return 0;
 }

802606938e6441754232a14ab92a0c393fdd6476
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2023-06-13 20:57:33 +0200

    Addition

---
+++ LAB_3_sem.2/Lab3.cpp
@@ -0,0 +1,133 @@
+﻿#include<iostream>
+
+class Complex {
+private:
+    double real;
+    double imag;
+public:
+    Complex(double r = 0, double i = 0) 
+    {
+        real = r;
+        imag = i;
+    }
+
+    // Конструктор копирования
+    Complex(const Complex& other) : real(other.real), imag(other.imag) {}
+
+    Complex operator=(const Complex& other) // Оператор присваивания копированием
+    {
+        real = other.real;
+        imag = other.imag;
+        return *this;
+        /* без* this было бы
+        Complex operator=(const Complex & other){
+            Complex a;
+            a.real = other.real;
+            a.imaginary = other.imaginary;
+            return a; 
+        }*/
+    }
+
+    ~Complex() {} // Деструктор
+
+    // оператор +=
+    Complex operator+=(const Complex& other)
+    {
+        real += other.real;
+        imag += other.imag;
+        return *this;
+    }
+
+    // оператор +
+    friend Complex operator+(Complex c1, const Complex& c2) 
+    {
+        return c1 += c2;
+    }
+
+    // оператор *=
+    Complex operator*=(const Complex& other) 
+    {
+        double r = real * other.real - imag * other.imag;
+        double i = real * other.imag + imag * other.real;
+        real = r;
+        imag = i;
+        return *this;
+    }
+
+    // оператор *
+    friend Complex operator*(Complex c1, const Complex& c2) 
+    {
+        return c1 *= c2;
+    }
+
+    // префиксный оператор ++
+    Complex& operator++() 
+    {
+        ++real;
+        return *this;
+    }
+
+    // постфиксный оператор ++
+    Complex operator++(int) 
+    {
+        Complex temp(*this);
+        ++(*this);
+        return temp;
+    }
+
+    // оператор ввода
+    friend std::istream& operator>>(std::istream& in, Complex& other) 
+    {
+        std::cout << "Введите действительную часть: ";
+        in >> other.real;
+        std::cout << "Введите мнимую часть: ";
+        in >> other.imag;
+        return in;
+    }
+
+    // оператор вывода
+    friend std::ostream& operator<<(std::ostream& out, const Complex& other)
+    {
+        if (other.imag >= 0)
+        {
+            out << other.real << " + " << other.imag << "i";
+            return out;
+        }
+        else
+        {
+            out << other.real << " - " << abs(other.imag) << "i";
+            return out;
+        }
+    }
+};
+
+int main() 
+{
+    setlocale(LC_ALL, "russian");
+    Complex a(2, 3);
+    Complex b(4, 5);
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    Complex c = a + b;
+    std::cout << "a + b = " << c << std::endl;
+
+    a += b;
+    std::cout << "a += b: a = " << a << std::endl;
+
+    Complex d = a * b;
+    std::cout << "a * b = " << d << std::endl;
+
+    a *= b;
+    std::cout << "a *= b: a = " << a << std::endl;
+
+    std::cout << "++a = " << ++a << std::endl;
+    std::cout << "a++ = " << a++ << std::endl;
+    std::cout << "a = " << a << std::endl;
+
+    Complex e;
+    std::cin >> e;
+    std::cout << "Вы ввели: " << e << std::endl;
+
+    return 0;
+}
---
+++ praktika_3/main.cpp
@@ -0,0 +1,119 @@
+﻿#include <chrono>
+#include <thread>
+#include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#define PI acos(-1)
+
+class KinematicSolver 
+{
+private:
+    double L1;
+    double L2;
+    double alpha;
+    double beta;
+public:
+    KinematicSolver(double newL1, double newL2)
+    {
+        L1 = newL1;
+        L2 = newL2;
+    }
+
+    void solve(double x, double y)
+    {
+        double B = sqrt(x * x + y * y);
+        alpha = 180 * (acos(x / B) + acos((L1 * L1 - L2 * L2 + B * B) / (2 * B * L1))) / PI;
+        beta = 180 * (PI - acos((L1 * L1 + L2 * L2 - B * B) / (2 * L1 * L2))) / PI;
+        if (y < 0) {
+            alpha *= -1;
+            beta *= -1;
+        }
+    }
+
+    void setJointsParams(double newL1, double newL2)
+    {
+        L1 = newL1;
+        L2 = newL2;
+    }
+
+    double getAlpha() const
+    {
+        return alpha;
+    }
+
+    double getL1() const
+    {
+        return L1;
+    }
+
+    double getL2() const
+    {
+        return L2;
+    }
+
+    double getBeta() const
+    {
+        return beta;
+    }
+};
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode(1000, 500), "praktika_3");
+
+
+    KinematicSolver sample(100, 49);
+
+    sf::Vector2f mousePosition;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::MouseMoved) 
+            {
+                mousePosition = sf::Vector2f(event.mouseMove.x, event.mouseMove.y);
+                double x = mousePosition.x - 500;
+                double y = 250 - mousePosition.y;
+                sample.solve(x, y);
+            }
+        }
+
+        window.clear();
+
+        sf::RectangleShape oX(sf::Vector2f(500.f, 1.f));
+        oX.rotate(90.f);
+        oX.setPosition(500, 0);
+        window.draw(oX);
+        sf::RectangleShape oY(sf::Vector2f(1000.f, 1.f));
+        oY.setPosition(0, 250);
+        window.draw(oY);
+
+        sf::RectangleShape joint1(sf::Vector2f(sample.getL1(), 1.f));
+        joint1.setPosition(500, 250);
+        joint1.setFillColor(sf::Color::Green);
+        sf::RectangleShape joint2(sf::Vector2f(sample.getL2(), 1.f));
+        joint2.setFillColor(sf::Color::Red);
+
+        joint1.rotate(-sample.getAlpha());
+        
+        joint2.setPosition(500.f + sample.getL1() * cos(sample.getAlpha() * PI / 180.f),
+            250.f - sample.getL1() * sin(sample.getAlpha() * PI / 180.f));
+        joint2.rotate(-sample.getAlpha() + sample.getBeta());
+        window.draw(joint1);
+        window.draw(joint2);
+        joint1.rotate(sample.getAlpha());
+        joint2.rotate(sample.getAlpha() - sample.getBeta());
+
+        window.display();
+    }
+
+    return 0;
+}
+
+
+
